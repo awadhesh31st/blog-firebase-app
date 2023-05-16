01ec4048f8664d8695adc0b052d95ea64876f236
awadhesh31st <awadhesh31st@gmail.com>
2023-05-16 12:03:33 +0530

    blog update code and date

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { DateTime } from "@/types/firebase";
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -5,11 +6,20 @@ export function cm(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(input: string | number): string {
-  const date = new Date(input);
-  return date.toLocaleDateString("en-US", {
+export const formatDate = (timestamp: DateTime): string => {
+  const seconds = timestamp?.seconds || 0;
+  const nanoseconds = timestamp?.nanoseconds || 0;
+  const milliseconds: number =
+    seconds * 1000 + Math.floor(nanoseconds / 1000000);
+  const date: Date = new Date(milliseconds);
+
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: "UTC",
     month: "long",
     day: "numeric",
     year: "numeric",
-  });
-}
+  };
+  const utcDateString: string = date.toLocaleString("en-US", options);
+
+  return utcDateString;
+};
